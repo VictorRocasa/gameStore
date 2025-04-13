5741b32fcdaa53a06e778b990f66403da9e27e90
Victor Capobianco <victorcapobianco01@yahoo.com.br>
2025-04-13 10:59:13 -0300

    [UPDATE] genresSeeder

--- GameStore/Api/Data/GameStoreContext.cs
+++ GameStore/Api/Data/GameStoreContext.cs
@@ -8,4 +8,15 @@ public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbCo
 {
     public DbSet<Game> Games => Set<Game>();
     public DbSet<Genre> Genres => Set<Genre>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new {Id = 1, Name = "Luta"},
+            new {Id = 2, Name = "RPG"},
+            new {Id = 3, Name = "Esporte"},
+            new {Id = 4, Name = "Corrida"},
+            new {Id = 5, Name = "Simulação"}
+        );
+    }
 }
