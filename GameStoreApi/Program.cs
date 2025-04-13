a4517c98f61f53f557f51ab6a27bc777ffc0de5c
Victor Capobianco <victorcapobianco01@yahoo.com.br>
2025-04-12 21:01:16 -0300

    [CREATE] endpoint para criar um jogo

--- GameStoreApi/Program.cs
+++ GameStoreApi/Program.cs
@@ -3,6 +3,8 @@ using GameStoreApi.Dtos;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+const string GetGameEndpointName = "GetGame";
+
 List<GameDto> games = [
     new (
         1,
@@ -27,10 +29,24 @@ List<GameDto> games = [
     ),
 ];
 
+app.MapGet("/", () => "Hello World!");
+
 app.MapGet("games", () => games);
 
-app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id)).WithName(GetGameEndpointName);
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("games", (CreateGameDto newGame) => {
+   GameDto game = new(
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+   ); 
+
+   games.Add(game);
+
+   return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+});
 
 app.Run();
